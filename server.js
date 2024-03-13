bbd1b678f679e5816650fa8dfca3780aef4b1a0d
sachin jayakody <Sachi.jayakody98@gmail.com>
2024-03-13 17:19:58 +1100

    Update One

---
+++ server.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let port = 3000;
+
+app.use(express.static(__dirname+'/'));
+app.get('/',(req,res) => {
+    res.render('index.html');
+});
+
+app.get('/addTwoNumbers', (req, res) => {
+    let num1 = req.query.num1;
+    let num2 = req.query.num2;
+
+    let result = parseInt(num1) + parseInt(num2); 
+
+    let response = {data: result, statusCode: 200, message: 'success'}; 
+
+    res.json(response);
+});
+
+app.listen(port, () => {
+    console.log("server started!");
+});
